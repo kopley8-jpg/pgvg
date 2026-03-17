babce56356a5aacbce0cc38ad1c704c1dcdc3279
Kopley8-jpg <Kopley8@gmail.com>
2026-03-17 11:42:40 +0300

    ekat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000, // форсим нужный порт
+    strictPort: true, // если 3000 занят - выкинет ошибку, а не переключится на другой
+  },
   plugins: [react()],
-})
+});
